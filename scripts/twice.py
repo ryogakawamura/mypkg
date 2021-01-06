c8f0feaf0af8855fbb2464f4d2a8eaa9cbe9674e
ryogakawamura <75007917+ryogakawamura@users.noreply.github.com>
2021-01-06 18:56:13 +0900

    Update twice.py

--- scripts/twice.py
+++ scripts/twice.py
@@ -1,7 +1,8 @@
+#!/usr/bin/env python3
+
 # SPDX-LICENSE-Identifer: BSD-2-Clause
 # Copyright (C) 2020 Ryuichi Ueda. All rights reserved.
 
-#!/usr/bin/env python3
 import rospy
 from std_msgs.msg import Int32
 
