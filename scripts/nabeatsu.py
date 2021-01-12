bb4526c8cb77bc0fe14c0539d9dd80e36f0d55fd
ryogakawamura <75007917+ryogakawamura@users.noreply.github.com>
2021-01-12 14:16:45 +0900

    Update nabeatsu.py

--- scripts/nabeatsu.py
+++ scripts/nabeatsu.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
 
 import rospy
-from std_msgs.msg import Int32
-
-n = 0
+from std_msgs.msg import String
 
 def cb(message):
 	global n
-	n = message.data*3
+	n = message.data
+	
+	if 3 in n:
+		word = 'San!!!!'
+	else
+		word = '......'
 
 rospy.init_node('nabe')
 sub = rospy.Subscriber('count_up', Int32, cb)
-pub = rospy.Publisher('nabeatsu', Int32, queue_size=1)
+pub = rospy.Publisher('nabeatsu', String, queue_size=1)
 rate = rospy.Rate(1)
 while not rospy.is_shutdown():
-	pub.publish(n)
+	pub.publish(word)
 	rate.sleep()
