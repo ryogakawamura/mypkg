4b415d12210aa7e4fe368b05391407dfc7237dc9
ryogakawamura <75007917+ryogakawamura@users.noreply.github.com>
2021-01-12 13:32:41 +0900

    Update nabeatsu.py

--- scripts/nabeatsu.py
+++ scripts/nabeatsu.py
@@ -6,14 +6,13 @@ from std_msgs.msg import Int32
 n = 0
 
 def cb(message):
-	global n
-	if 3 in (message.data):
-		n = 333333
+    global n
+    n = message.data*3
 
 rospy.init_node('nabe')
 sub = rospy.Subscriber('count_up', Int32, cb)
 pub = rospy.Publisher('nabeatsu', Int32, queue_size=1)
-rate = rospy.Rate(10)
+rate = rospy.Rate(100)
 while not rospy.is_shutdown():
 	pub.publish(n)
 	rate.sleep()
