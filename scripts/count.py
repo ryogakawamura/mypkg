34e7c9b61e6954cac7375009df9ab0ab95f3668b
ryogakawamura <75007917+ryogakawamura@users.noreply.github.com>
2021-01-12 13:38:59 +0900

    Update count.py

--- scripts/count.py
+++ scripts/count.py
@@ -8,6 +8,6 @@ pub = rospy.Publisher('count_up', Int32, queue_size=1)
 rate = rospy.Rate(1)
 n = 0
 while not rospy.is_shutdown():
-    n += 1
-    pub.publish(n)
-    rate.sleep()
+	n += 1
+	pub.publish(n)
+	rate.sleep()
